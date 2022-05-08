ec871cb6c1f9ae806eb6a6b465cbfe3cfd912d53
Wijnand Karsens <info@codefromanywhere.com>
2022-05-08 11:48:15 +0200

    update api broken files

--- apps/docs/src/util/api.ts
+++ apps/docs/src/util/api.ts
@@ -1,7 +1,7 @@
 import { AllEndpoints } from "core";
 import { makeApi } from "sensible-core";
 import { NO_API_SELECTED } from "../constants";
-
+//
 /**
  * we need a different way to get the api here,
  * since we are using the URL to get the endpoint instead of the env files.
--- packages/ui/src/api.ts
+++ packages/ui/src/api.ts
@@ -4,5 +4,5 @@ import env from "../env.json";
 import { isDev } from "./util/isDev";
 const { localServer, remoteServer, runLocalServer } = env;
 const apiUrl = isDev && runLocalServer ? localServer : remoteServer;
-
+////
 export const api = makeApi<AllEndpoints>({ apiUrl });
