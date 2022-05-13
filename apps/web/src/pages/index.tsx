0e079be78265f0c143663702cc09bd90dab88ae2
Wijnand Karsens <info@codefromanywhere.com>
2022-05-13 17:21:37 +0200

    style

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -7,7 +7,7 @@ const CTA = () => {
   return (
     <a href="/docs/docs/about" rel="noreferrer" className="my-10">
       <button
-        className={`text-white disabled:hidden transition-all duration-600 w-40 lg:w-96 bg-blue-500 hover:bg-blue-400 border-t-blue-300 border-b-blue-700 border rounded-xl p-4`}
+        className={`cursor-pointer text-white disabled:hidden transition-all duration-600 w-40 lg:w-96 bg-blue-500 hover:bg-blue-400 border-t-blue-300 border-b-blue-700 border rounded-xl p-4`}
       >
         {"Read the docs"}
       </button>
